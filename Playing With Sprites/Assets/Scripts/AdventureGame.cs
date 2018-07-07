f1e36b69253685ea38c27e12e214c9e76fcbf438
Shaowei <zswkiller@gmail.com>
2018-07-07 20:12:35 +1000

    update the states and fix error for out of range issue

---
+++ Playing With Sprites/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+//    [SerializeField] is used to link variable to Component in Font end.
+    [SerializeField] private Text _textComponent;
+
+    [SerializeField] private State _startingState;
+
+    private State _state;
+
+    // Use this for initialization
+    void Start()
+    {
+        _state = _startingState;
+        _textComponent.text = _state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = _state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (nextStates.Length == 2)
+            {
+                _state = nextStates[1];
+            }
+        }
+
+//        else if (Input.GetKeyDown(KeyCode.Alpha3))
+//        {
+//            _state = nextStates[2];
+//        }
+        _textComponent.text = _state.GetStateStory();
+    }
+}
---
+++ Playing With Sprites/Assets/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//use to create state in Unity
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    //TextArea with min size 10, 14 lines start to scroll
+    [TextArea(10, 14)] [SerializeField] private string _storyText;
+    [SerializeField] private State[] _nextStates;
+
+    public string GetStateStory()
+    {
+        return _storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return _nextStates;
+    }
+}
